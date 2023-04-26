6bc142856d8a577f16370e2290b7673360f61cbe
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-27 02:41:02 +0530

    [Rohan]Add: Comparing two lines with equals method

--- Line_Comparison/Line_Comparison/Program.cs
+++ Line_Comparison/Line_Comparison/Program.cs
@@ -11,10 +11,16 @@ namespace Line_Comparison
         static void Main(string[] args)
         {
             Console.WriteLine("//////LINE COMPARISON//////");
-            CalculateLength length_One = new CalculateLength(1, 2, 3, 4);
-            length_One.Calculate();
+            CalculateLength line_One = new CalculateLength(1, 2, 3, 4);
+            double length_One = line_One.Calculate();
 
+            CalculateLength line_two = new CalculateLength(5, 6, 7, 6);
+            double length_two = line_two.Calculate();
 
+            if (length_One.Equals(length_two))
+                Console.WriteLine("Lines are equal");
+            else
+                Console.WriteLine("Lines are not equal");
             Console.ReadKey();
         }
     }
