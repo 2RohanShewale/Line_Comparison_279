9cc0e330b2a376f4b87f8b86b0d9ad8477b615c6
Rohan Shewale <shewalerohan50@gmail.com>
2023-04-27 02:37:16 +0530

    [Rohan]Add: Calculating the legth of a Line

---
+++ Line_Comparison/Line_Comparison/CalculateLength.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison
+{
+    public class CalculateLength
+    {
+        int lengthX, _lengthY, lengthsX, lengthsY;
+
+        public CalculateLength(int lengthX, int lengthY, int lengthsX, int lengthsY)
+        {
+            this.lengthX = lengthX;
+            _lengthY = lengthY;
+            this.lengthsX = lengthsX;
+            this.lengthsY = lengthsY;
+        }
+        public double Calculate()
+        {
+            //lenght = sqrt((X2-x1)^2 + (Y2 - y1)^2);
+            double length = Math.Sqrt(Math.Pow((lengthsX - lengthX), 2) + Math.Pow((lengthsY - _lengthY), 2));
+            Console.WriteLine(length);
+            return length;
+        }
+    }
+}
---
+++ Line_Comparison/Line_Comparison/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("//////LINE COMPARISON//////");
+            CalculateLength length_One = new CalculateLength(1, 2, 3, 4);
+            length_One.Calculate();
+
+
+            Console.ReadKey();
+        }
+    }
+}
